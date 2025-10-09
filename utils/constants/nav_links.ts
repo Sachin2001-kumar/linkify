b6fb06a918e842eadaa12f7d6fc1f855de490a96
Sachin Kumar Dagar <sachinkumardagar20001@gmail.com>
2025-10-09 16:08:46 +0530

    Add nav_links

---
+++ utils/constants/nav_links.ts
@@ -0,0 +1,53 @@
+import {
+  HelpCircleIcon,
+  LineChartIcon,
+  Link2Icon,
+  NewspaperIcon,
+  QrCodeIcon,
+} from "lucide-react";
+export const NAV_LINKS = [
+  {
+    title: "features",
+    href: "/features",
+    menu: [
+      {
+        title: "Link Shortening",
+        tagline: "Shorten links and track their performance.",
+        href: "/features/link-shortening",
+        icon: Link2Icon,
+      },
+      {
+        title: "Advanced Analytics",
+        tagline: "Gain insights into who is clicking your links.",
+        href: "/features/analytics",
+        icon: LineChartIcon,
+      },
+    ],
+  },
+  {
+    title: "Pricing",
+    href: "./pricing",
+  },
+  {
+    title: "Resources",
+    href: "/resources",
+    menu: [
+      {
+        title: "Blog",
+        tagline: "Read articles on the latest trends in tech.",
+        href: "/resources/blog",
+        icon: NewspaperIcon,
+      },
+      {
+        title: "Help",
+        tagline: "Get answers to your questions.",
+        href: "/resources/help",
+        icon: HelpCircleIcon,
+      },
+    ],
+  },
+  {
+    title: "Changelog",
+    href: "/changelog",
+  },
+];
