5e33245c60d83aa650c2ba109dcb5f740c3787d6
Sachin Kumar Dagar <sachinkumardagar20001@gmail.com>
2025-10-11 02:05:48 +0530

    Add Footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-import { Footer } from "@/components/Navigation/Footer";
+import Footer from "@/components/Navigation/Footer";
 import { Navbar } from "@/components/Navigation/Navbar";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
-      <Navbar />
+      {/* <Navbar /> */}
       <Footer />
     </>
   );
